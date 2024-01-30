c50e17c46374b34931ab1ed14bf17085ac264e11
Luis Blanco <ljblanco@redborder.com>
2024-01-30 08:59:23 +0000

    parametrized node name

--- resources/scripts/rb_check_memservices.rb
+++ resources/scripts/rb_check_memservices.rb
@@ -1,7 +1,9 @@
+require 'socket'
 module RedBorder
     module Checker
         def self.check_memservices_cgroups
-            memory_services=`knife node show rb-ljblanco -l -F json | jq '.default.redborder.memory_services | keys[]'`.chomp.lines
+            hostname = Socket.gethostname.split('.')[0]
+            memory_services=`knife node show #{hostname} -l -F json | jq '.default.redborder.memory_services | keys[]'`.chomp.lines
             memory_services.all? do |s|
               cgroup = `systemctl show -p ControlGroup #{s}`.gsub('ControlGroup=','').chomp
               s = s.delete("\",-").chomp
