5631ca28e2284415acdd8c02aaea4020efee6324
Luis Blanco <ljblanco@redborder.com>
2024-01-31 16:00:38 +0000

    run script as ruby by default, getting better hostname

--- resources/scripts/rb_check_cgroups.rb
+++ resources/scripts/rb_check_cgroups.rb
@@ -1,8 +1,18 @@
+#!/usr/bin/env ruby
+
 require 'socket'
+require 'chef'
 module RedBorder
     module Checker
         def self.check_memservices_cgroups
-            hostname = Socket.gethostname.split('.')[0]
+            # TODO: optimize node get with chef confifured
+            Chef::Log::info("Memservices Check")
+            # Chef::Config.from_file("/etc/chef/client.rb")
+            # Chef::Config[:client_key] = "/etc/chef/admin.pem"
+            # Chef::Config[:http_retry_count] = 5
+            hostname = `hostname -s`.strip
+            # node = Chef::Node.load(hostname)
+            # memory_services=node.default.memory_services.keys()
             memory_services=`knife node show #{hostname} -l -F json | jq '.default.redborder.memory_services | keys[]'`.chomp.lines
             active_services=memory_services.select do |s|
                 `systemctl is-active #{s}`.chomp == 'active'
