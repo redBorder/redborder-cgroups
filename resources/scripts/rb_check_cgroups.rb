8da0e4193d5eb3c139c85f187bd72dc693cf56c2
Luis Blanco <ljblanco@redborder.com>
2024-01-31 15:17:34 +0000

    i was supposing cgroup empty means inactive

--- resources/scripts/rb_check_cgroups.rb
+++ resources/scripts/rb_check_cgroups.rb
@@ -4,10 +4,13 @@ module RedBorder
         def self.check_memservices_cgroups
             hostname = Socket.gethostname.split('.')[0]
             memory_services=`knife node show #{hostname} -l -F json | jq '.default.redborder.memory_services | keys[]'`.chomp.lines
-            memory_services.all? do |s|
+            active_services=memory_services.select do |s|
+                `systemctl is-active #{s}` == 'active'
+            end
+            active_services.all? do |s|
               cgroup = `systemctl show -p ControlGroup #{s}`.gsub('ControlGroup=','').chomp
               s = s.delete("\",-").chomp
-              cgroup.empty? || cgroup.include?("redborder-#{s}.slice") #assigned cgroup should cointain redborder-webui ie, else false
+              cgroup.include?("redborder-#{s}.slice") #assigned cgroup should cointain redborder-webui ie, else false
             end
         end
     end
