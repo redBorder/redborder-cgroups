5ca794348c81e259096d764faadd764337e80b45
Luis Blanco <ljblanco@redborder.com>
2024-02-26 15:20:28 +0000

    lint more def less lines in one

--- resources/scripts/rb_check_cgroups.rb
+++ resources/scripts/rb_check_cgroups.rb
@@ -10,15 +10,24 @@ module RedBorder
   module Checker
     def self.check_memservices_cgroups
       Chef::Log::info("Memservices Check")
-      hostname = `hostname -s`.strip
-      memory_services=`knife node show #{hostname} -l -F json | jq '.default.redborder.memory_services | keys[]'`.chomp.lines
-      active_services=memory_services.select do |s|
-          `systemctl is-active #{s}`.chomp == 'active'
-      end
-      active_services.all? do |s|
+      active_memory_services.all? do |s|
         cgroup = `systemctl show -p ControlGroup #{s}`.gsub('ControlGroup=','').chomp
         s = s.delete("\",-").chomp
         cgroup.include?("redborder-#{s}.slice") #assigned cgroup should cointain redborder-webui.slice ie, else false
+      end
+    end
+
+    def self.hostname 
+      `hostname -s`.strip
+    end
+
+    def self.memory_services
+      `knife node show #{hostname} -l -F json | jq '.default.redborder.memory_services | keys[]'`.chomp.lines
+    end
+
+    def self.active_memory_services
+      memory_services.select do |s|
+        `systemctl is-active #{s}`.chomp == 'active'
       end
     end
   end
